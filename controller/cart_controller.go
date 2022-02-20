0ee04fa09fa527c086bb94c4215354eb35c96e3f
levinnivel <nivelmart12@gmail.com>
2022-02-20 21:10:50 +0700

    admin & cart controllers

---
+++ controller/cart_controller.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	"encoding/json"
+	"log"
+
+	model "DailyFresh-Backend/model"
+)
+
+func addGoods() {
+	
+}
+
+func removeGoods() {
+	
+}
+
+func checkout() {
+	
+}
