612c7b746b6ce7ee09c8bd2ba4e2977a12ec651c
levinnivel <nivelmart12@gmail.com>
2022-04-11 00:01:18 +0700

    singleton db

--- database/db_handler.go
+++ database/db_handler.go
@@ -6,9 +6,15 @@ import (
 )
 
 func Connect() *sql.DB {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/daily_fresh?parseTime=true&loc=Asia%2FJakarta")
-	if err != nil {
-		log.Fatal(err)
+	var db *sql.DB
+	var err error
+
+	if db == nil && err == nil {
+		db, err = sql.Open("mysql", "root:@tcp(localhost:3306)/daily_fresh?parseTime=true&loc=Asia%2FJakarta")
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
+
 	return db
 }
