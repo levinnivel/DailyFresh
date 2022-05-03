43449f2deed7cbcb80c2d5fffa3e2aff2010ea52
levinnivel <nivelmart12@gmail.com>
2022-05-03 20:41:13 +0700

    fix path issue

--- domain/controller/user/admin_controller.go
+++ domain/controller/user/admin_controller.go
@@ -1,74 +1,74 @@
 package controller
 
-import (
-	dbHandler "DailyFresh-Backend/database"
-	model "DailyFresh-Backend/domain/user/model"
-	rsp "DailyFresh-Backend/response"
-	"log"
+// import (
+// 	dbHandler "DailyFresh-Backend/database"
+// 	model "DailyFresh-Backend/domain/user/model"
+// 	rsp "DailyFresh-Backend/response"
+// 	"log"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-gonic/gin"
+// )
 
-// Ban Account...
-func BanAccount(c *gin.Context) {
-	db := dbHandler.Connect()
-	defer db.Close()
+// // Ban Account...
+// func BanAccount(c *gin.Context) {
+// 	db := dbHandler.Connect()
+// 	defer db.Close()
 
-	userId := c.Param("user_id")
+// 	userId := c.Param("user_id")
 
-	_, errQuery := db.Exec("DELETE FROM user WHERE id=?",
-		userId,
-	)
+// 	_, errQuery := db.Exec("DELETE FROM user WHERE id=?",
+// 		userId,
+// 	)
 
-	var response model.UserResponse
-	if errQuery == nil {
-		response.Message = "Delete User Success"
-		rsp.SendUserSuccessResponse(c, response)
-	} else {
-		response.Message = "Delete User Failed Error"
-		rsp.SendUserErrorResponse(c, response)
-	}
-}
+// 	var response model.UserResponse
+// 	if errQuery == nil {
+// 		response.Message = "Delete User Success"
+// 		rsp.SendUserSuccessResponse(c, response)
+// 	} else {
+// 		response.Message = "Delete User Failed Error"
+// 		rsp.SendUserErrorResponse(c, response)
+// 	}
+// }
 
-func getOrderHistory() {
+// func getOrderHistory() {
 
-}
+// }
 
-func getGoods() {
+// func getGoods() {
 
-}
+// }
 
-func updateOrder() {
+// func updateOrder() {
 
-}
+// }
 
-// Reply Ticket
-func ReplyTicket(c *gin.Context) {
-	db := dbHandler.Connect()
-	defer db.Close()
+// // Reply Ticket
+// func ReplyTicket(c *gin.Context) {
+// 	db := dbHandler.Connect()
+// 	defer db.Close()
 
-	id := c.Param("id")
-	reply := c.PostForm("reply")
+// 	id := c.Param("id")
+// 	reply := c.PostForm("reply")
 
-	rows, _ := db.Query("SELECT * FROM ticket WHERE id='" + id + "'")
-	var ticket model.Ticket
-	for rows.Next() {
-		if err := rows.Scan(&ticket.ID, &ticket.Category, &ticket.Inquiry, &ticket.Reply, &ticket.UserID); err != nil {
-			log.Fatal(err.Error())
-		}
-	}
+// 	rows, _ := db.Query("SELECT * FROM ticket WHERE id='" + id + "'")
+// 	var ticket model.Ticket
+// 	for rows.Next() {
+// 		if err := rows.Scan(&ticket.ID, &ticket.Category, &ticket.Inquiry, &ticket.Reply, &ticket.UserID); err != nil {
+// 			log.Fatal(err.Error())
+// 		}
+// 	}
 
-	_, errQuery := db.Exec("UPDATE ticket SET reply = ? WHERE id=?",
-		reply,
-		id,
-	)
+// 	_, errQuery := db.Exec("UPDATE ticket SET reply = ? WHERE id=?",
+// 		reply,
+// 		id,
+// 	)
 
-	var response model.TicketResponse
-	if errQuery == nil {
-		response.Message = "Reply Ticket Success"
-		rsp.SendTicketSuccessResponse(c, response)
-	} else {
-		response.Message = "Reply Ticket Failed Error"
-		rsp.SendTicketErrorResponse(c, response)
-	}
-}
+// 	var response model.TicketResponse
+// 	if errQuery == nil {
+// 		response.Message = "Reply Ticket Success"
+// 		rsp.SendTicketSuccessResponse(c, response)
+// 	} else {
+// 		response.Message = "Reply Ticket Failed Error"
+// 		rsp.SendTicketErrorResponse(c, response)
+// 	}
+// }
