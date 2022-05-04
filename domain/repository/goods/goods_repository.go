20e4a44b7b6e6821065edb75317f7f7e9e908f2b
levinnivel <nivelmart12@gmail.com>
2022-05-04 21:32:32 +0700

    customer seller get, register, update (incorrect)

--- domain/repository/goods/goods_repository.go
+++ domain/repository/goods/goods_repository.go
@@ -1,1 +1,71 @@
 package repository
+
+import (
+	"log"
+
+	dbHandler "DailyFresh-Backend/database"
+	Model "DailyFresh-Backend/domain/model/goods"
+)
+
+// GetGoods...
+func GetGoods(id string) []Model.Goods {
+	db := dbHandler.Connect()
+	defer db.Close()
+
+	query := "SELECT * from goods"
+
+	if id != "" {
+		query += " WHERE id='" + id + "'"
+	}
+
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var goods Model.Goods
+	var goodies []Model.Goods
+	for rows.Next() {
+		if err := rows.Scan(&goods.ID, &goods.Name, &goods.Price, &goods.Description,
+			&goods.Stock, &goods.Image, &goods.SellerID); err != nil {
+			log.Fatal(err.Error())
+		} else {
+			goodies = append(goodies, goods)
+		}
+	}
+
+	return goodies
+}
+
+// GetGoods...
+func GetGoodsBySellerId(id string) []Model.Goods {
+	db := dbHandler.Connect()
+	defer db.Close()
+
+	query := "SELECT goods.name, goods.price, goods.description, goods.stock, goods.image, " +
+		"seller.seller_address" +
+		"FROM goods JOIN seller ON goods.seller_id = seller.user_id " +
+		"WHERE type_person='customer'"
+
+	if id != "" {
+		query += " WHERE id='" + id + "'"
+	}
+
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var goods Model.Goods
+	var goodies []Model.Goods
+	for rows.Next() {
+		if err := rows.Scan(&goods.ID, &goods.Name, &goods.Price, &goods.Description,
+			&goods.Stock, &goods.Image, &goods.SellerID); err != nil {
+			log.Fatal(err.Error())
+		} else {
+			goodies = append(goodies, goods)
+		}
+	}
+
+	return goodies
+}
